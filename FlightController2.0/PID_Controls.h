cd01e0cecd8ccd099afcefa6c3e21453323f3621
Yihia <alshekly@outlook.com>
2021-03-06 23:02:38 -0500

    Create PID_Controls.h

---
+++ FlightController2.0/PID_Controls.h
@@ -0,0 +1,60 @@
+
+
+class PID_Controls {
+  public:
+    //control
+
+    int period = 50;  //Refresh rate period of the loop is 50ms
+
+
+
+
+    double val (float p, float i, float d, float intended_Val, float actual_Val, int period) {
+
+      float error = intended_Val - actual_Val;
+      float old_Error = error - 1;// ?
+
+
+      float pid_p = error * p;
+      float pid_i = pid_i + (i * error);
+      float pid_d = (d * old_Error / period);
+
+      float PID_out = pid_p + pid_i + pid_d;
+      PID_out * -1;
+
+      if (PID_out < 2000) {
+        //PID_out = 2000;
+      }
+      if (PID_out > 1000) {
+        //PID_out = 1000;
+      }
+
+      return PID_out;
+      old_Error = error;
+    }
+
+};
+
+
+
+
+
+/*
+  PID Class
+  a specific class for controlling UAV control_surfaces
+  """
+
+  """
+  how to tune the PID values manually
+    increase the d until the position (error) is stable (not changing a lot)
+    increase the p slowly  until the error is almost to 0 (+or- 5ish%)
+    increase the i by relatively very small increment until the error is 0
+
+        # the PID values
+    # proportional for the present error
+    p = 8
+    # integrator for the past error
+    i = 0.1
+    # derivative for the future error
+    d = 15
+*/
